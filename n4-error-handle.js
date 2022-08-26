6ddb0de27ef7d58ac39156a9493907dc234c852b
Sazzad ur Rahman <22joylobo@gmail.com>
2022-08-26 19:45:54 +0600

    error-handling

---
+++ n4-error-handle.js
@@ -0,0 +1,26 @@
+async function getData() {
+  try {
+    // undefined.find();
+    const customError = new Error("Email already exist");
+    throw customError;
+  } catch (error) {
+    // console.log(error);
+    //this is how we find errors
+    //in production , one send the error to a globally declared errorHandler function as a parameter;
+    errorHandler(error);
+  }
+  console.log("done 2 ~~");
+}
+getData();
+
+function errorHandler(error) {
+  const { name, message, stack } = error;
+  //industry level e error guloke logger e record kore rakha hoy.
+  // logger.error({
+  //   name,
+  //   message,
+  //   stack
+  // })
+  console.log(name,message)
+}
+
